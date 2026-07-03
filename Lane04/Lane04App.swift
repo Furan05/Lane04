77e79ac1a710d5deee923cc7ccd237773dc8558e
Francois Dubois <francois.dubois1994@gmail.com>
2026-07-03 21:26:32 +0200

    Initial Commit

---
+++ Lane04/Lane04App.swift
@@ -0,0 +1,17 @@
+//
+//  Lane04App.swift
+//  Lane04
+//
+//  Created by François.Dubois on 03/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Lane04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lane04Tests/Lane04Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lane04Tests.swift
+//  Lane04Tests
+//
+//  Created by François.Dubois on 03/07/2026.
+//
+
+import Testing
+@testable import Lane04
+
+struct Lane04Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
